facab777ab769e1b0e39880b0f4aa20f677cd1a9
Patrik_Jørgensen <vadmand806@gmail.com>
2024-09-16 09:24:04 +0200

    dotenv opsat samt begge providers

---
+++ client/src/Providers/SupabaseProvider.jsx
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js"
+import React, { createContext, useContext, useEffect, useState } from "react"
+
+const SupabaseContext = createContext()
+
+export const SupabaseProvider = ({ children }) => {
+  const [supabase, setSupabase] = useState(null)
+
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+  useEffect(() => {
+    setSupabase(createClient(supabaseUrl, supabaseKey))
+  },[supabaseKey])
+
+  return (
+    <SupabaseContext.Provider value={{ supabase }}>
+      {children}
+    </SupabaseContext.Provider>
+  )
+}
+
+export const useSupabase = () => useContext(SupabaseContext)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SupabaseProvider } from './Providers/SupabaseProvider.jsx'
+import { AuthProvider } from './Providers/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
+    <SupabaseProvider>
     <App />
+    </SupabaseProvider>
+    </AuthProvider>
   </StrictMode>,
 )
